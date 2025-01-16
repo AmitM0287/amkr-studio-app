b6dc1b00567b8449ea4b84ff747ba1efcebc04e7
Amit Manna <amitmanna@AmitM-KrsnaR-Macbook-Air.local>
2025-01-16 23:34:37 +0530

    on click on avatar show dropdown options added

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -13,12 +13,22 @@ const Navbar: React.FC<NavbarProps> = ({ onMenuClick }) => {
             className="py-2 px-3 grid grid-cols-12 sm:grid-cols-12 bg-gray-100 items-center shadow sticky top-0 z-10"
         >
             <MenuIcon className="col-span-11 sm:col-span-11 text-gray-500 cursor-pointer" onClick={onMenuClick} />
-            <div className="avatar justify-self-end">
-                <Image
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                    alt="avatar"
-                    cls="w-8 rounded-full cursor-pointer"
-                />
+            <div className="avatar dropdown dropdown-end col-span-1 sm:col-span-1 justify-self-end">
+                <div className="avatar" role="button" tabIndex={0}>
+                    <Image
+                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                        alt="avatar"
+                        cls="w-8 rounded-full cursor-pointer"
+                    />
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu bg-gray-100 rounded-box z-[1] w-52 mt-4 p-2 shadow">
+                    <li>
+                        <a>Item 1</a>
+                    </li>
+                    <li>
+                        <a>Item 2</a>
+                    </li>
+                </ul>
             </div>
         </div>
     );
